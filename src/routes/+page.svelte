0f9ca9adf947d1fe37901104a41c8403535b329c
eriknelson2 <eriknelson818@gmail.com>
2024-08-30 11:03:40 -0400

    Intial Layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,66 @@
-<h1>Hello World</h1>
 
-<button class="btn btn-neutral">Neutral</button>
+<div class="justify-center items-center h-screen">
+    <div class="gray text-primary-content grid place-content-center rounded">
+        <p color="black">Brim Advisory</p>
+    </div>
+    <div class="medium-blue text-accent-content grid place-content-center rounded">
+        <p>Bruce McGoogan is a seasoned leader with more than 25 years of global experience in the food
+and feed industries. He specializes in bridging technology and business, leading R&amp;D strategy,
+innovation, and business development. Bruce&#39;s career is marked by his ability to forge strong
+external partnerships and collaborate effectively across internal functions, driving significant
+business potential and growth from technology. His educational background includes a PhD in
+Nutrition from Texas A&amp;M University, an MS in Aquaculture from Louisiana State University, and
+a BS in Biology from University of North Carolina–Wilmington.
+In his recent role as VP Global Business Development at Cargill, Bruce led enterprise business
+strategy and investment projects ranging from a few million dollars to over $1.5 billion. His
+tenure at Cargill also includes roles such as Strategy and Innovation Director, Strategy and
+Business Development Lead in Europe, and Food Technology Leader in North America and
+Asia-Pacific. His contributions have significantly impacted strategic marketing, innovation
+portfolios, and R&amp;D leadership, demonstrating his expertise in driving technological
+advancements and business success.</p>
+    </div>
+    <div class="dark-blue text-secondary-content grid place-content-center rounded">
+      Consulting Focus Areas:
+      <br>
+      - Business Strategy
+      <br>
+      -Market/Competitor Intelligence and Opportunity Evaluation
+      
+    </div>
+</div>
+
+<div>
+
+</div>
+<div>
+
+</div>
+<div>
+
+</div>
+<footer class="footer bg-neutral text-neutral-content p-10">
+    <nav>
+        <h6 class="footer-title">Contact Information</h6>
+        <p>Address: Fernandina Beach, Florida, USA</p>
+        <p>Phone: (952)-338-7597</p>
+        <p>Email: mcgoogs1@gmail.com</p>
+    </nav>
+</footer>
+
+<style>
+    .flex-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        justify-content: center;
+    }
+    .dark-blue {
+        background-color: #344373;
+    }
+    .gray {
+        background-color: #D9D7D7;
+    }
+    .medium-blue {
+        background-color: #646E8C;
+    }
+</style>
